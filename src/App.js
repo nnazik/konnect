0bec5ae9f0d750ffe6cc8996fc8aa12a3a516463
ishan <ishans@insharptechnologies.com>
2019-10-06 19:46:16 +0530

    update laterst files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import '../src/assets/css/main.css'
 import { Switch, Route } from 'react-router-dom';
-import ResetPassword from './containers/ResetPassword';
 import Login from './containers/Login';
 import CreateEvent from './containers/CreateEvent';
 import DashboardEvent from './containers/DashboardEvent';
@@ -27,7 +26,6 @@ function App() {
         <Route exact path='/ResetPassword2' component={ResetPassword2} />
         <Route exact path='/ResetPassword3' component={ResetPassword3} />
         <Route exact path='/CompanyTitle/:id' component={CompanyTitle} />
-        <Route exact path='/resetpassword' component={ResetPassword} />
         <Route exact path='/createevent' component={CreateEvent} />
         <Route exact path='/dashboard' component={DashboardEvent} />
         <Route exact path='/platformsearch' component={PlatformSearch} />
